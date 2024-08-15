f518a9a1016d0794b9da024bd597eade27fa3e42
JulieKodehode <julie.kodehode@gmail.com>
2024-08-15 12:02:44 +0200

    Added JS funcionallity to have a unit converter

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*
-- Hente knappen og sette opp en hendelse
+- Hente knappen og sette opp en hendelse X
 - Hente input verdi fra nettsiden
 - Hente valg verdi
 - Gjøre utrekning
@@ -9,7 +9,62 @@
 function unitConverter(event) {
     event.preventDefault()
     
-    console.log("Virker du?")
+    const unitInput = parseFloat(document.querySelector("#unitInput").value);
+    // console.log(unitInput);
+    // console.log(typeof unitInput);
+
+    const unitFrom = document.querySelector("#unitFrom").value;
+    // console.log(unitFrom);
+    const unitTo = document.querySelector("#unitTo").value;
+    // console.log(unitTo);
+    let displayResult = document.querySelector("#displayResult");
+    // console.log(displayResult);
+
+    if (unitFrom === "MM" && unitTo === "CM" ) {
+        // Conversion: Millimeters to Centimeters
+        displayResult.textContent = unitInput / 10;
+    } else if (unitFrom === "MM" && unitTo === "M") {
+        // Conversion: Millimeters to Meters
+        displayResult.textContent = unitInput / 1000;
+    } else if (unitFrom === "MM" && unitTo === "KM") {
+        // Conversion: Millimeters to Kilometers
+        displayResult.textContent = unitInput / 1000000;
+    } else if (unitFrom === "CM" && unitTo === "MM") {
+        // Conversion: Centimeters to Millimeters
+        displayResult.textContent = unitInput * 10;
+    } else if (unitFrom === "CM" && unitTo === "M") {
+        // Conversion: Centimeters to Meters
+        displayResult.textContent = unitInput / 100;
+    } else if (unitFrom === "CM" && unitTo === "KM") {
+        // Conversion: Centimeters to Kilometers
+        displayResult.textContent = unitInput / 100000;
+    } else if (unitFrom === "M" && unitTo === "MM") {
+        // Conversion: Meters to Millimeters
+        displayResult.textContent = unitInput * 1000;
+    } else if (unitFrom === "M" && unitTo === "CM") {
+        // Conversion: Meters to Centimeters
+        displayResult.textContent = unitInput * 100;
+    } else if (unitFrom === "M" && unitTo === "KM") {
+        // Conversion: Meters to Kilometers
+        displayResult.textContent = unitInput / 1000;
+    } else if (unitFrom === "KM" && unitTo === "MM") {
+        // Conversion: Kilometers to Millimeters
+        displayResult.textContent = unitInput * 1000000;
+    } else if (unitFrom === "KM" && unitTo === "CM") {
+        // Conversion: Kilometers to Centimeters
+        displayResult.textContent = unitInput * 100000;
+    } else if (unitFrom === "KM" && unitTo === "M") {
+        // Conversion: Kilometers to Meters
+        displayResult.textContent = unitInput * 1000;
+    } else {
+        // MM til MM
+        // CM til CM
+        // M til M
+        // KM til KM
+        // Error
+        displayResult.textContent = "Velg to ulike målenheter.";
+        displayResult.style.color = "red";
+    }
 }
 
 const convertButton = document.querySelector("#convertButton");
